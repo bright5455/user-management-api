52ee08dead444c1f19fe214110e431b570e93571
Anwanga-Abasi Frriday <brightidio377@gmail.com>
2025-10-10 12:27:51 +0100

    Add changes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT!;
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? process.env.PORT;
+  if (port === undefined) {
+    throw new Error('PORT is not defined in config or environment variables');
+  }
   await app.listen(port, () => {
-    console.log(`🚀 Server running on port ${port}`);
+    console.log(`Server is running on ${port}`);
   });
+  
 }
 bootstrap();
